eb692513750a26a4bfa4f7b6fb5c13378b1fa499
hthomander <tho17074@byui.edu>
2025-03-07 14:52:47 -0700

    try

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,13 @@
-const menuToggle = document.querySelector('.hamburger');
-const menu = document.getElementById('menu');
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.hamburger');
+    const menu = document.getElementById('menu');
+
+    console.log(menuToggle);
+    console.log(menu);
 
 menuToggle.addEventListener("click", function () {
+    console.log("Hamburger button clicked!");
     menu.classList.toggle("show");
+    
 });
-
+});
